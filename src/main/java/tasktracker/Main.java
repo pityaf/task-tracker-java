7e2b47dd1cec9f10092168bfaf90d53cac50b2db
Paolo Cantoreggi <paolo.cantoreggi@gmail.com>
2025-05-04 20:39:42 +0200

    add task tracker main

--- src/main/java/tasktracker/Main.java
+++ src/main/java/tasktracker/Main.java
@@ -1,11 +1,18 @@
 package tasktracker;
 
+import tasktracker.app.TaskTrackerApp;
+
 /**
  *
  * @author pityafinwe
  */
 public class Main {
-    public static void main(String[] args) {
+    
+    public static void main(String args[]) {
         System.out.println("Inizializing...");
+        
+        TaskTrackerApp taskTracker = new TaskTrackerApp();
+        taskTracker.start();
     }
+    
 }
