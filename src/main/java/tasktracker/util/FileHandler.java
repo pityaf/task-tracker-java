f06b47cd90a66d9ff2f2a7057c1bce345f8c0311
Paolo Cantoreggi <paolo.cantoreggi@gmail.com>
2025-05-18 16:34:12 +0200

    improve read csv handler

--- src/main/java/tasktracker/util/FileHandler.java
+++ src/main/java/tasktracker/util/FileHandler.java
@@ -1,5 +1,6 @@
 package tasktracker.util;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Paths;
@@ -22,21 +23,33 @@ public class FileHandler {
     
     public FileHandler() {
         this.fileName = "./src/main/java/tasktracker/data/task-data.csv";
-        try {
-            this.scanner = new Scanner(Paths.get(this.fileName));
-            this.writer = new PrintWriter(this.fileName);
-        } catch(IOException e) {
-            System.out.println("Unable to open file. " + e.getMessage());
-        }
+        //this.writer = new PrintWriter(this.fileName);
     } 
     
     public Map<Integer, Task> readFromCsv() {
         HashMap<Integer, Task> tasks = new HashMap<>();
-        // read header
-        this.scanner.nextLine();
-        while(this.scanner.hasNextLine()) {
-            String[] task = this.scanner.nextLine().split(",");
-            tasks.put(Integer.valueOf(task[0]), new Task(Integer.parseInt(task[0]), task[1], TaskStatus.valueOf(task[2]), task[3], task[4]));
+        File file = new File(this.fileName);
+
+        // check if file is empty
+        if(!file.exists() || file.length() == 0) {
+            System.out.println("File is empty.");
+            return tasks;	
+        }
+
+        try {
+            this.scanner = new Scanner(Paths.get(this.fileName));
+            
+            if(this.scanner.hasNextLine()) {
+                this.scanner.nextLine(); //read header
+
+                while(this.scanner.hasNextLine()) {
+                String[] task = this.scanner.nextLine().split(",");
+                tasks.put(Integer.valueOf(task[0]), new Task(Integer.parseInt(task[0]), task[1], TaskStatus.valueOf(task[2]), task[3], task[4]));
+                }
+
+            }
+        } catch(IOException e) {
+            System.out.println("Unable to open file. " + e.getMessage());
         }
         return tasks;
     }
