3246c85f402ef97741e317094ba22dc3cb755efb
Paolo Cantoreggi <paolo.cantoreggi@gmail.com>
2025-08-21 09:44:17 +0200

    add methods to read and delete csv elements

--- src/main/java/tasktracker/util/FileHandler.java
+++ src/main/java/tasktracker/util/FileHandler.java
@@ -1,6 +1,7 @@
 package tasktracker.util;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.HashMap;
@@ -18,11 +19,15 @@ public class FileHandler {
     
     private final String fileName;
     private Scanner scanner;
-    //private PrintWriter writer;
+    private FileWriter writer;
     
     public FileHandler() {
         this.fileName = "./src/main/java/tasktracker/data/task-data.csv";
-        //this.writer = new PrintWriter(this.fileName);
+        try {
+            this.writer = new FileWriter(fileName, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     } 
     
     public Map<Integer, Task> readFromCsv() {
@@ -54,7 +59,40 @@ public class FileHandler {
     }
     
     public void addToCsv(Task task) {
-        
+        try {
+            this.writer.append("\n");
+            this.writer.append(
+                task.getID() + "," +
+                task.getTitle() + "," +
+                task.getStatus() + "," +
+                task.getCreationTime() + "," +
+                task.getUpdateTime()
+            );
+            this.writer.flush();
+        } catch (IOException e) {
+            System.out.println("File not found...");
+        }
+    }
+
+    public void addToCsv(Map<Integer, Task> tasks) {
+        try {
+            this.writer = new FileWriter(this.fileName, false);
+            this.writer.write("id,title,status,createdAt,updatedAt");
+            this.writer.append("\n");
+            for(int id : tasks.keySet()) {
+                this.writer.append(
+                    tasks.get(id).getID() + "," +
+                    tasks.get(id).getTitle() + "," +
+                    tasks.get(id).getStatus() + "," +
+                    tasks.get(id).getCreationTime() + "," +
+                    tasks.get(id).getUpdateTime()
+                );
+                this.writer.append("\n");
+            }
+            this.writer.flush();
+        } catch (IOException e) {
+            System.out.println("File not found...");
+        }
     }
     
     public void saveToCsv(Map<Integer, Task> tasks) {
@@ -63,6 +101,18 @@ public class FileHandler {
     
     public void addToCsv() {
         
+    }
+
+    public void removeFromCsv(Task task) {
+        Map<Integer, Task> tasks = this.readFromCsv();
+        HashMap<Integer, Task> updatedTasks = new HashMap<>();
+
+        for(int id : tasks.keySet()) {
+            if(!tasks.get(id).getTitle().equals(task.getTitle())) {
+                updatedTasks.put(id, tasks.get(id));
+            }
+        }
+        this.addToCsv(updatedTasks);
     }
     
 }
