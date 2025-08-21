9271fa6217ebdbbc352c9d082acd836c08328d2e
Paolo Cantoreggi <paolo.cantoreggi@gmail.com>
2025-08-21 09:44:51 +0200

    add sort, filter, delete options

--- src/main/java/tasktracker/app/TaskTrackerApp.java
+++ src/main/java/tasktracker/app/TaskTrackerApp.java
@@ -1,8 +1,10 @@
 package tasktracker.app;
 
+import java.util.Map;
 import java.util.Scanner;
 
 import tasktracker.model.Task;
+import tasktracker.model.TaskStatus;
 import tasktracker.service.TaskService;
 import tasktracker.util.FileHandler;
 import tasktracker.util.UIHelper;
@@ -63,16 +65,89 @@ public class TaskTrackerApp {
                     }
                 }
                 case "delete" -> {
+                    if(input.length < 2) {
+                        System.out.println("Usage: delete <id>");
+                        break;
+                    }
+                    int deleteID = Integer.parseInt(input[1]);
+                    Task task = this.taskService.getTasks().get(deleteID);
+
+                    if(task != null) {
+                        this.fileHandler.removeFromCsv(task);
+                        this.taskService.initTasks(this.fileHandler.readFromCsv());
+                    } else {
+                        System.out.println("Task do not exists");
+                    }
                 }
                 case "status" -> {
+                    // status of task given the id
+                    if(input.length < 2) {
+                        System.out.println("Usage: status <id>");
+                        break;
+                    }
+                    try {
+                        int statusID = Integer.parseInt(input[1]);
+                        Task task = this.taskService.getTasks().get(statusID);
+                        if (task != null) {
+                            this.UI.printFormatted(task);
+                        } else {
+                            System.out.println("Task with ID " + statusID + " not found");
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid ID format: " + input[1]);
+                    }
                 }
                 case "update" -> {
+                    // update status given the id
+                    if(input.length < 2) {
+                        System.out.println("Usage: update <id>");
+                        break;
+                    }
+                    try {
+                        int statusID = Integer.parseInt(input[1]);
+                        TaskStatus status = this.taskService.getTasks().get(statusID).getStatus();
+                        this.taskService.getTasks().get(statusID).setStatus(status.next());
+                        this.fileHandler.addToCsv(this.taskService.getTasks());
+
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid ID format: " + input[1]);
+                    }
                 }
                 case "edit"-> {
+                    // edit task giveen the id
                 }
                 case "filter" -> {
+                    // base on status
+                    if(input.length < 2) {
+                        System.out.println("Usage: filter <id>");
+                        break;
+                    }
+                    
+                    TaskStatus statusType = TaskStatus.TODO;;
+
+                    try {
+                        statusType = TaskStatus.valueOf(input[1]); 
+                    } catch (IllegalArgumentException e) {
+                        System.out.println("Illegal status: " + input[1]);
+                    }
+
+                    Map<Integer, Task> tasks = this.taskService.getTasks();
+                    for(int id : tasks.keySet()) {
+                        if(tasks.get(id).getStatus() == statusType) {
+                            this.UI.printFormatted(tasks.get(id));
+                        }
+                    }
                 }
                 case "sort" -> {
+                      Map<Integer, Task> tasks = this.taskService.getTasks();
+
+                    for (TaskStatus statusType : TaskStatus.values()) {
+                        for(int id : tasks.keySet()) {
+                            if(tasks.get(id).getStatus() == statusType) {
+                                this.UI.printFormatted(tasks.get(id));
+                            }
+                        }
+                    }
                 }
                 case "help" -> {
                     this.UI.printHelp();
